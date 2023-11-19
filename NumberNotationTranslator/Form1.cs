dbbfe90e11320bdc57c2fad83878f47d3f87b49c
LEX1005001 <AleX69.galkin@yandex.ru>
2023-11-19 14:31:38 +0300

    Добавьте файлы проекта.

---
+++ NumberNotationTranslator/Form1.cs
@@ -0,0 +1,83 @@
+﻿using NumberNotationTranslatorClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumberNotationTranslator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Событие нажатия на кнопку
+        /// </summary>
+        /// <param name="sender">Элемент управления, который вызвал событие</param>
+        /// <param name="e">Параметры события</param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            private void button1_Click(object sender, EventArgs e)
+            {
+                string xStr;
+                if (textBoxInputNumber.Text != "")
+                {
+                    xStr = textBoxInputNumber.Text;
+                }
+                else
+                {
+                    MessageBox.Show("Некорректный ввод.\n+" +
+                        "Поле \"Введите число\" не может быть пустым.");
+                    return;
+                }
+                bool Negative = false;//Забираем минус у числа если оно отриц
+                if (xStr[0] == Convert.ToChar("-"))
+                {
+                    Negative = true;
+                    xStr = xStr.Remove(0, 1);
+                }
+            }
+
+            private NotationTranslationHandler GetValues(NotationTranslationHandler translationHandler)
+            {
+                try
+                {
+                    double inputNumber = double.Parse(textBoxInputNumber.Text);
+                    int notationFrom = int.Parse(textBoxNotationFrom.Text);
+                    int notationTo = int.Parse(textBoxNotationTo.Text);
+
+                    translationHandler =
+                            new NotationTranslationHandler(inputNumber,
+                            notationFrom, notationTo);
+
+
+                    return translationHandler;
+                }
+                catch
+                {
+                    MessageBox.Show("Неверно указаны значения цветов RGB", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return null;
+                }
+
+            }
+
+            private void textBoxInputNumber_TextChanged(object sender, EventArgs e)
+            {
+
+            }
+
+            private void button2_Click(object sender, EventArgs e)
+            {
+
+            }
+        }
+    }
---
+++ NumberNotationTranslatorClassLibrary/NotationTranslationHandler.cs
@@ -0,0 +1,59 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberNotationTranslatorClassLibrary
+{
+    /// <summary>
+    /// Класс для обработки перевода чисел из одной системы счисления в другую.
+    /// Класс предназначени для подготовки числа к переводу
+    /// </summary>
+   *//* public class NotationTranslationHandler
+    {
+       *//*
+
+
+        public NotationTranslationHandler(){}
+*//*
+        public NotationTranslationHandler(double number, int notationFrom, int notationTo)
+        {
+            int intPart = (int)Math.Truncate(number);
+            //подумать как брать дробную часть.
+            //Можно использовать строку. Отбросить "0," и остальное брать как дробную часть
+            int fractPart = (int)(number - (int)Math.Truncate(number));
+*//*
+            integerTranslator = new NotationTranslator(intPart, notationFrom, notationTo);
+            fractTranslator = new NotationTranslator(fractPart, notationFrom, notationTo);
+
+        }
+
+
+        public int Translate()
+        {
+
+            int intAnswer = integerTranslator.FromNotationFromToDecimal();
+            //1)Если число в 10 системе счисления то делим на нужную систему счисления
+            //2)Иначе переводим число в 10 систему счисления и делим на на нужную систему счисления
+            
+            integerTranslator.Number = intAnswer;
+
+            intAnswer = integerTranslator.FromDecimalToNotationTo();
+
+
+            integerTranslator.Number = intAnswer; 
+            
+            int fractAnswer = fractTranslator.FromNotationFromToDecimal();
+            fractTranslator.Number = fractAnswer;
+
+            fractAnswer = fractTranslator.FromDecimalToNotationTo();
+            fractTranslator.Number = fractAnswer;
+
+            return integerTranslator.Number+ fractTranslator.Number;
+        }
+
+
+    }
+}
+*/
